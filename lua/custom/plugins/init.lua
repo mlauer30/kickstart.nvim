930d0190579178c126856d4d0709041cb16862ad
mlauer30 <mlauer07@gmail.com/>
2026-01-22 13:31:18 -0500

    added codeium plugin to build

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,13 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  'Exafunction/windsurf.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+  },
+  config = function()
+    require('codeium').setup {}
+  end,
+}
