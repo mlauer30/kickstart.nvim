2b307ee6b58867d81ebe1fc92c5ffdad84488b2f
mlauer30 <mlauer07@gmail.com/>
2026-01-30 13:24:34 -0500

    markdown renderer plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,23 +3,34 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter',
-  config = function()
-    -- Optional: disable default bindings if you want custom ones
-    vim.g.codeium_disable_bindings = 1
-    -- Custom keymaps (optional)
-    vim.keymap.set('i', '<C-g>', function()
-      return vim.fn['codeium#Accept']()
-    end, { expr = true })
-    vim.keymap.set('i', '<C-;>', function()
-      return vim.fn['codeium#CycleCompletions'](1)
-    end, { expr = true })
-    vim.keymap.set('i', '<C-,>', function()
-      return vim.fn['codeium#CycleCompletions'](-1)
-    end, { expr = true })
-    vim.keymap.set('i', '<C-x>', function()
-      return vim.fn['codeium#Clear']()
-    end, { expr = true })
-  end,
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter',
+    config = function()
+      -- Optional: disable default bindings if you want custom ones
+      vim.g.codeium_disable_bindings = 1
+      -- Custom keymaps (optional)
+      vim.keymap.set('i', '<C-g>', function()
+        return vim.fn['codeium#Accept']()
+      end, { expr = true })
+      vim.keymap.set('i', '<C-;>', function()
+        return vim.fn['codeium#CycleCompletions'](1)
+      end, { expr = true })
+      vim.keymap.set('i', '<C-,>', function()
+        return vim.fn['codeium#CycleCompletions'](-1)
+      end, { expr = true })
+      vim.keymap.set('i', '<C-x>', function()
+        return vim.fn['codeium#Clear']()
+      end, { expr = true })
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
